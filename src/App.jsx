288ee8167db47d7c9d9850eea3bf4451cbe64320
N.Panhamony <mac@Macs-MacBook-Pro.local>
2025-09-04 22:17:47 +0700

    Add Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-
+import Card from "./Card.jsx"
+import "./index.css"
 function App() {
-  
+    return(
+        <>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        </>
+    )
 }
 
 export default App
